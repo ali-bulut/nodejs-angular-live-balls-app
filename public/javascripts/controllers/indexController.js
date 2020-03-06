bdc30eb0412efb85e8ccadbca08c0285a205dff7
Ali Bulut <alibulut@yahoo.com>
2020-03-06 15:37:23 +0300

    newUser event was created

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,12 +1,44 @@
-app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) =>{
-    const connectionOptions={
-        reconnectionAttempts:3,
-        reconnectionDelay:600
+app.controller('indexController', ($scope, indexFactory) =>{
+    //$scope'tan sonraki kısım view kısmında kullanacağımız function'ın adını belirler.
+
+    $scope.messages=[]
+
+
+    $scope.init=() => {
+        const username=prompt('Please enter the username');
+
+        if(username)
+            initSocket(username);
+        else
+            return false;
     }
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-    .then((socket)=>{
-        console.log('Successfully connected!', socket);
-    }).catch((err) => {
-        console.log(err);
-    })
-}])
+
+    function initSocket(username) {
+
+        const connectionOptions={
+            reconnectionAttempts:3,
+            reconnectionDelay:600
+        }
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        .then((socket)=>{
+            socket.emit('newUser', {username:username});
+
+            socket.on('newUser', (data)=>{
+                const messageData={
+                    type:0, // info (sadece sistem tarafından oluşturulan giriş çıkış mesajları)
+                    username:data.username
+                }
+                $scope.messages.push(messageData);
+
+                //push gibi DOM eventleri kullandığımız için apply methodunu çalıştırmalıyız ki angular'ın
+                //haberi olsun
+                $scope.$apply();
+            })
+        }).catch((err) => {
+            console.log(err);
+        })
+
+    }
+
+    
+})
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,11 +2,29 @@ const socketio=require('socket.io');
 const io=socketio();
 
 const socketApi={};
-
 socketApi.io=io;
+
+const users=[];
 
 io.on('connection', (socket)=>{
     console.log('User Connected');
+
+    //datadan username gelir.
+    socket.on('newUser', (data)=>{
+        const defaultData={
+            id: socket.id,
+            position:{
+                x:0,
+                y:0
+            }
+        }
+
+        //iki objeyi birleştirmek için assign methodu kullanılır.
+        const userData=Object.assign(data,defaultData);
+        users.push(userData);
+
+        socket.broadcast.emit('newUser', (userData));
+    })
 })
 
 module.exports=socketApi;
